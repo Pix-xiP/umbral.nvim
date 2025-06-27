99d1dc5899cded7150f271f6ea087901eac4927c
Pix xiP <pix119@proton.me>
2025-06-27 10:53:05 +0800

    added 'supermaven'

---
+++ lua/umbral/groups/supermaven.lua
@@ -0,0 +1,15 @@
+local M = {}
+
+M.url = "https://github.com/supermaven-inc/supermaven-nvim"
+
+---@type umbral.HighlightsFn
+function M.get(c, opts)
+	_ = opts
+  -- stylua: ignore
+  local ret = {
+    SupermavenSuggestion = { fg = c.terminal.black },
+  }
+	return ret
+end
+
+return M
