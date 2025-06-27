3e3a79b674928f0689d49dfd4ae3e371644601f0
Pix xiP <pix119@proton.me>
2025-06-27 17:45:00 +0800

    added 'codeium'

---
+++ lua/umbral/groups/codeium.lua
@@ -0,0 +1,16 @@
+local M = {}
+
+M.url = "https://github.com/Exafunction/codeium.nvim"
+
+---@type umbral.HighlightsFn
+function M.get(c, opts)
+	_ = opts
+  -- stylua: ignore
+  local ret = {
+    CodeiumSuggestion = { fg = c.terminal.black},
+  }
+
+	return ret
+end
+
+return M
